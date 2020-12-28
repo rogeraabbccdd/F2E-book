86e16900f7ca38718c27c7c654ceab2a01453257
rogeraabbccdd <rogeraabbccdd@gmail.com>
2020-12-28 16:07:46 +0800

    新增 PDF 遷入元件

---
+++ docs/.vuepress/components/PDF.vue
@@ -0,0 +1,24 @@
+<template>
+  <div ref="pdf"></div>
+</template>
+
+<style>
+.pdfobject-container { 
+  height: 30rem;
+  border: 1rem solid rgba(0,0,0,.1);
+}
+</style>
+
+<script>
+import PDFObject from 'pdfobject'
+
+export default {
+  name: 'PDF',
+  props: {
+    src: String
+  },
+  mounted () {
+    PDFObject.embed(this.src, this.$refs.pdf)
+  }
+}
+</script>
