b0fdcd147d160302676d9d698ed29c9f647d417b
rogeraabbccdd <rogeraabbccdd@gmail.com>
2024-03-28 21:51:00 +0800

    修正 mindmap 元件

---
+++ docs/components/Mindmap.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :id="mindData.options.container" class="mindmap"></div>
+</template>
+
+<style lang="sass">
+.mindmap
+  width: 100%
+  height: 450px
+  overflow: auto
+  
+jmnode
+  font: unset !important
+</style>
+
+<script setup>
+import jsMind from "jsmind"
+import "jsmind/style/jsmind.css"
+import { onMounted } from 'vue'
+
+const props = defineProps({
+  mindData: {
+    type: Object
+  }
+})
+
+onMounted(() => {
+  const jm = new jsMind(props.mindData.options);
+  jm.show(props.mindData.mind);
+})
+</script>
