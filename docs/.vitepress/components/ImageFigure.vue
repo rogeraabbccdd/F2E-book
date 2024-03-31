e7999204ea01261256d36c7bbb1d58b977d11f9d
rogeraabbccdd <rogeraabbccdd@gmail.com>
2024-03-31 20:56:58 +0800

    新增 ImageFigure 元件

---
+++ docs/.vitepress/components/ImageFigure.vue
@@ -0,0 +1,45 @@
+<template>
+  <figure class="image-figure">
+    <template v-if="!link">
+      <img :src="realSrc" :alt="alt" :title="title">
+      <figcaption>
+        <slot></slot>
+      </figcaption>
+    </template>
+    <template v-else>
+      <a :href="link" target="_blank">
+        <img :src="realSrc" :alt="alt" :title="title">
+      </a>
+      <a :href="link" target="_blank">
+        <figcaption>
+          <slot></slot>
+        </figcaption>
+      </a>
+    </template>
+  </figure>
+</template>
+
+<script setup>
+import { withBase } from 'vitepress'
+import { computed } from 'vue'
+
+const props = defineProps({
+  src: {
+    type: String,
+    required: true
+  },
+  alt: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String
+  }
+})
+
+const realSrc = computed(() => withBase(props.src))
+</script>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -3,6 +3,7 @@ import FlowChart from '../components/FlowChart.vue'
 import Mindmap from '../components/Mindmap.vue'
 import PDF from '../components/PDF.vue'
 import Tree from '../components/Tree.vue'
+import ImageFigure from '../components/ImageFigure.vue'
 import $ from 'jquery'
 import './custom.sass'
 
@@ -13,6 +14,7 @@ export default {
     app.component('Mindmap', Mindmap)
     app.component('PDF', PDF)
     app.component('Tree', Tree)
+    app.component('ImageFigure', ImageFigure)
     if (!import.meta.env.SSR) {
       window.$ = $
     }
